f6003a47fa70b85594fe65947db3be213116a8e9
BretJohnson1 <155486952+BretJohnson1@users.noreply.github.com>
2024-10-21 14:22:39 -0500

    logged body, firstname and lastname to the console

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,20 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  let body;
+  let fristName;
+  let lastName;
+
+  return uploadPhoto()
+  .then((data) => {
+    body = data.body;
+    createUser()
+    .then((data) => {
+      fristName = data.fristName;
+      lastName = data.lastName;
+      console.log('${body} $(firstName) ${lastName}');
+    })
+    .catch(() => console.log('Signup system offline'));
+  })
+  .catch(() => console.log('Signup system offline'));
+}
