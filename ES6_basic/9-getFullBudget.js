f5320c4508741a239e8b60e433408f0fe05c43df
BretJohnson1 <155486952+BretJohnson1@users.noreply.github.com>
2024-10-28 14:55:19 -0500

    full budget

---
+++ ES6_basic/9-getFullBudget.js
@@ -0,0 +1,14 @@
+import getBudgetObject from './7-getBudgetObject';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeDollars(income) {
+      return '$${income}';
+    },
+    getIncomeInEuros(income) {
+      return '${income} euros';
+    },
+  };
+}
