aa60d2d8f96ccf034726380a6e6d9118b0a4e037
BretJohnson1 <155486952+BretJohnson1@users.noreply.github.com>
2024-10-28 15:04:16 -0500

    report object

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,8 @@
+export default function createReportObject(employeeList) {
+  return {
+    allEmployees: {...employeeList },
+    getNumberOfDepartments(employeeList) {
+      return Object.keys(employeeList).length;
+    },
+  };
+}
