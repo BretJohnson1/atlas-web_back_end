b190f6e8b6811ff373b46990d628b4d0f82bd1c5
BretJohnson1 <155486952+BretJohnson1@users.noreply.github.com>
2024-10-30 10:53:46 -0500

    minor changes to block scoped

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,9 +3,10 @@ export default function taskBlock(trueOrFalse) {
   var task2 = true;
 
   if (trueOrFalse) {
-    var task = true;
-    var task2 = false;
+    const task = true;
+    const task2 = false;
+    console.log(task + task2);
   }
 
-  return [task, task2]
+  return [task, task2];
 }
